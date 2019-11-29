e7a68d865a48e8b9c0c15bf5214de611b1376271
Duban Camilo Bedoya Jimenez <duban.bedoya@globant.com>
2019-11-29 15:53:18 -0500

    Code refactor

--- src/main/java/CountingValleys.java
+++ src/main/java/CountingValleys.java
@@ -1,42 +1,71 @@
 public class CountingValleys {
 
-    static final int MINIMUM_STEPS = 2;
-    static final int MAXIMUM_STEPS = 1000000;
-
-    public CountingValleys(){
-
-    }
+    private static final int MINIMUM_STEPS = 2;
+    private  static final int MAXIMUM_STEPS = 1000000;
 
     // Complete the countingValleys function below.
-    static int countingValleys(int n, String s) {
+    static int countValleys(int n, String s) {
         int numberOfValleys = 0;
         int seaLevel = 0;
         int path = 0;
         boolean isAValley = false;
-        if(n < MINIMUM_STEPS || n > MAXIMUM_STEPS) {
+        if (n < MINIMUM_STEPS || n > MAXIMUM_STEPS) {
             System.out.println("Steps must be between 0 and 1,000,000");
-        } else {
-            for (int i = 0; i < n ; i++) {
-                if(s.charAt(i) == 'U'){
-                    if(path == 0){
-                        isAValley = false;
-                    }
-                    path++;
-                } else if(s.charAt(i) == 'D'){
-                    if(path == 0){
-                        isAValley = true;
-                    }
-                    path--;
-                } else {
-                    System.out.println("Path must only contain 'U' or 'D' character");
-                    break;
-                } if(path == seaLevel){
-                    if(isAValley){
-                        numberOfValleys++;
-                    }
+            return numberOfValleys;
+        }
+        if (n != s.length()) {
+            System.out.println("'n' does not match with s length");
+            return numberOfValleys;
+        }
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'U') {
+                if (path == 0) {
+                    isAValley = false;
                 }
+                path++;
+            } else if (s.charAt(i) == 'D') {
+                if (path == 0) {
+                    isAValley = true;
+                }
+                path--;
+            } else {
+                System.out.println("Path must only contain 'U' or 'D' character");
+                numberOfValleys = 0;
+                break;
+            }
+            if (path == seaLevel && isAValley) {
+                    numberOfValleys++;
             }
         }
+        return numberOfValleys;
+    }
+
+    static int countingValleys(int n, String s){
+        int numberOfValleys = 0;
+        int seaLevel = 0;
+        int path = 0;
+        boolean isAValley = false;
+        if (n < MINIMUM_STEPS || n > MAXIMUM_STEPS) {
+            System.out.println("Steps must be between 0 and 1,000,000");
+            return numberOfValleys;
+        }
+        if (n != s.length()) {
+            System.out.println("'n' does not match with s length");
+            return numberOfValleys;
+        }
+        for (int i = 0; i < n ; i++) {
+            if(s.charAt(i) == 'U'){
+                path++;
+                if(path == seaLevel ){
+                    numberOfValleys++;
+                }
+            } else if(s.charAt(i) == 'D') {
+                path--;
+            } else {
+                return 0;
+            }
+        }
+
         return numberOfValleys;
     }
 }
--- src/test/java/CountingValleysTest.java
+++ src/test/java/CountingValleysTest.java
@@ -4,29 +4,43 @@ import static org.junit.Assert.assertEquals;
 
 public class CountingValleysTest {
 
-    CountingValleys countingValleys;
+    private CountingValleys countingValleys;
 
-    public CountingValleysTest(){}
+    public CountingValleysTest() {
+    }
 
     @Before
-    public void setup(){
+    public void setup() {
         countingValleys = new CountingValleys();
     }
 
-
     @Test
-    public void shouldReturnZero(){
-        assertEquals(0, countingValleys.countingValleys(5,"UDDDU"));
+    public void shouldReturnZero() {
+        assertEquals(0, CountingValleys.countingValleys(5, "UDDDU"));
     }
 
     @Test
-    public void shouldReturnOne(){
-        assertEquals(1, countingValleys.countingValleys(8,"UDDDUDUU"));
+    public void shouldReturnOne() {
+        assertEquals(1, CountingValleys.countingValleys(8, "UDDDUDUU"));
     }
 
     @Test
-    public void shouldReturnTwo(){
-        assertEquals(2, countingValleys.countingValleys(10,"UDDDUDUUDU"));
+    public void shouldReturnTwo() {
+        assertEquals(2, CountingValleys.countingValleys(10, "UDDDUDUUDU"));
     }
 
+    @Test
+    public void shouldReturnOneHundred() {
+        assertEquals(100, CountingValleys.countingValleys(200, "DUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDU"));
+    }
+
+    @Test
+    public void whenThereIsAnIncorrectSymbolShouldReturnZero(){
+        assertEquals(0, CountingValleys.countingValleys(2, "UA"));
+    }
+
+    @Test
+    public void whenLengthDoesNotMatchWithStringShouldReturnZero(){
+        assertEquals(0, CountingValleys.countingValleys(10, "UD"));
+    }
 }
