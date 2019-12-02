4105a194d811a240e7ec7941bf683145f5566467
Duban Camilo Bedoya Jimenez <duban.bedoya@globant.com>
2019-12-02 09:09:34 -0500

    First challenge has been completed

---
+++ src/main/java/challenges/CountingValleys.java
@@ -0,0 +1,38 @@
+package challenges;
+
+import org.apache.log4j.Logger;
+
+class CountingValleys {
+
+    private static final Logger LOGGER = Logger.getLogger(CountingValleys.class.getName());
+    private static final int MINIMUM_STEPS = 2;
+    private  static final int MAXIMUM_STEPS = 1000000;
+
+    // Complete the countingValleys function below.
+    static int countValleys(int n, String s){
+        int numberOfValleys = 0;
+        int seaLevel = 0;
+        int path = 0;
+        if (n < MINIMUM_STEPS || n > MAXIMUM_STEPS) {
+            LOGGER.warn("Steps must be between 0 and 1,000,000");
+            return numberOfValleys;
+        }
+        if (n != s.length()) {
+            LOGGER.warn("'n' does not match with s length");
+            return numberOfValleys;
+        }
+        for (int i = 0; i < n ; i++) {
+            if(s.charAt(i) == 'U'){
+                path++;
+                if(path == seaLevel ){
+                    numberOfValleys++;
+                }
+            } else if(s.charAt(i) == 'D') {
+                path--;
+            } else {
+                return 0;
+            }
+        }
+        return numberOfValleys;
+    }
+}
--- src/test/java/challenges/CountingValleysTest.java
+++ src/test/java/challenges/CountingValleysTest.java
@@ -1,3 +1,6 @@
+package challenges;
+
+import challenges.CountingValleys;
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
@@ -16,31 +19,31 @@ public class CountingValleysTest {
 
     @Test
     public void shouldReturnZero() {
-        assertEquals(0, CountingValleys.countingValleys(5, "UDDDU"));
+        assertEquals(0, CountingValleys.countValleys(5, "UDDDU"));
     }
 
     @Test
     public void shouldReturnOne() {
-        assertEquals(1, CountingValleys.countingValleys(8, "UDDDUDUU"));
+        assertEquals(1, CountingValleys.countValleys(8, "UDDDUDUU"));
     }
 
     @Test
     public void shouldReturnTwo() {
-        assertEquals(2, CountingValleys.countingValleys(10, "UDDDUDUUDU"));
+        assertEquals(2, CountingValleys.countValleys(10, "UDDDUDUUDU"));
     }
 
     @Test
     public void shouldReturnOneHundred() {
-        assertEquals(100, CountingValleys.countingValleys(200, "DUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDU"));
+        assertEquals(100, CountingValleys.countValleys(200, "DUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDU"));
     }
 
     @Test
     public void whenThereIsAnIncorrectSymbolShouldReturnZero(){
-        assertEquals(0, CountingValleys.countingValleys(2, "UA"));
+        assertEquals(0, CountingValleys.countValleys(2, "UA"));
     }
 
     @Test
     public void whenLengthDoesNotMatchWithStringShouldReturnZero(){
-        assertEquals(0, CountingValleys.countingValleys(10, "UD"));
+        assertEquals(0, CountingValleys.countValleys(10, "UD"));
     }
 }
