84b3324c17de1cdc142e773746e6a22d3d6556e1
Duban Camilo Bedoya Jimenez <duban.bedoya@globant.com>
2019-12-05 16:41:00 -0500

    Added fifth challenge tests

---
+++ src/main/java/challenges/LeftRotation.java
@@ -0,0 +1,11 @@
+package challenges;
+
+public class LeftRotation {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int[] result = new int[10];
+
+        return result;
+    }
+}
---
+++ src/test/java/challenges/LeftRotationTest.java
@@ -0,0 +1,62 @@
+package challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeftRotationTest {
+
+    @Test
+    public void shouldReturnAnEmptyArrayWhenArrayLengthIsMoreThan100000(){
+        int[] array = new int[100001];
+        assertEquals(0, LeftRotation.rotLeft(array,2).length);
+    }
+
+    @Test
+    public void shouldReturnAnEmptyArrayWhenLeftRotationsNumberIsLessThanOne(){
+        int[] array = new int[3];
+        assertEquals(0, LeftRotation.rotLeft(array,-1).length);
+    }
+
+    @Test
+    public void shouldReturnAnEmptyArrayWhenLeftRotationsNumberIsMoreThanArrayLength(){
+        int[] array = new int[3];
+        assertEquals(0, LeftRotation.rotLeft(array,4).length);
+    }
+
+    @Test
+    public void shouldReturnAnEmptyArrayWhenAnyArrayElementIsLessThanOne(){
+        int[] array = {1,0,1};
+        assertEquals(0, LeftRotation.rotLeft(array,4).length);
+    }
+
+    @Test
+    public void shouldReturnAnEmptyArrayWhenAnyArrayElementIsMoreThanOneMillion(){
+        int[] array = {1,1_000_001,1};
+        assertEquals(0, LeftRotation.rotLeft(array,4).length);
+    }
+
+    @Test
+    public void shouldMoveArrayElementsOnce(){
+        int[] array = {1,2,3,4};
+        assertEquals(2, LeftRotation.rotLeft(array,1)[0]);
+    }
+
+    @Test
+    public void shouldMoveArrayElementsTwice(){
+        int[] array = {1,2,3,4};
+        assertEquals(3, LeftRotation.rotLeft(array,2)[0]);
+    }
+
+    @Test
+    public void shouldMoveArrayElementsThreeTimes(){
+        int[] array = {1,2,3,4};
+        assertEquals(4, LeftRotation.rotLeft(array,3)[0]);
+    }
+
+    @Test
+    public void shouldMoveArrayElementsFourTimes(){
+        int[] array = {1,2,3,4,5};
+        assertEquals(5, LeftRotation.rotLeft(array,4)[0]);
+    }
+}
